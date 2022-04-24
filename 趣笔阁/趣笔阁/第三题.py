73220dd3459ff428e874b991d0c65ec2be252ceb
秋安 <55935832+1600858489@users.noreply.github.com>
2022-04-24 16:40:31 +0800

    Add files via upload

---
+++ 趣笔阁/趣笔阁/第三题.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import  BeautifulSoup
+import re,time
+
+def main():
+    url = "https://www.bqkan8.com/50_50096/"
+    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.44"}
+    reponse = requests.get(url,headers = headers)
+    reponse.encoding = "gbk"
+    res = BeautifulSoup(reponse.text,features="html.parser")
+    res = res.select_one("body > div.listmain > dl > dd:nth-child(15) > a")
+    url_one = res.get("href")
+    one = requests.get("https://www.bqkan8.com" + str(url_one),headers = headers)
+    one.encoding = "gbk"
+    return  one.text
+    
+
+if __name__ == "__main__":
+    print(main())
